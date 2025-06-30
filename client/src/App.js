1918c561f2336a7549f01fcb43a6c0fcc3761519
Mahesh Bist <bistmahesh333@gmail.com>
2025-06-30 17:59:01 +0545

    react app setup

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from './Button';
+function App() {
+  return (
+    <div className="App">
+      <h1>hello react</h1>
+      {/* <Button title ="click"/>
+      <Button title ="add"/> */}
+  <button>{"add"}</button>
+    </div>
+  );
+}
+
+export default App;
